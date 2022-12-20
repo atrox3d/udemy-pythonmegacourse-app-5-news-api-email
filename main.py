5592ae6a104097b834b4ed479611257ebb5c07e3
atrox3d <atrox3d@gmail.com>
2022-12-20 16:30:34 +0100

    day 27: add links, specify language, limit news, control category

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests
 from secret.newsapi_apikey import API_KEY
 from api_cache import APICache
+import mailer
 
 
 def get_data(url, use_cache=False):
@@ -16,10 +17,29 @@ def get_data(url, use_cache=False):
     return data
 
 
-url = f'https://newsapi.org/v2/everything?q=tesla' \
-      f'&from=2022-11-20&sortBy=publishedAt' \
-      f'&apiKey={API_KEY}'
-
 if __name__ == '__main__':
+    topic = "tesla"
+    url = f'https://newsapi.org/v2/everything?' \
+          f'q={topic}' \
+          f'&from=2022-11-20' \
+          f'&language=en' \
+          f'&pageSize=20' \
+          f'&sortBy=publishedAt' \
+          f'&apiKey={API_KEY}'
+
     data = get_data(url, use_cache=True)
+    print(len(data['articles']))
+    print(data)
+
+    message = ""
+    for article in data['articles'][:20]:
+        title = article['title']
+        description = article['description']
+        link = article['url']
+
+        message += f'Title: {title}\n'
+        message += f'Description: {description}\n'
+        message += f'{link}\n\n'
+
+    mailer.send_mail("top news for today", message)
 
