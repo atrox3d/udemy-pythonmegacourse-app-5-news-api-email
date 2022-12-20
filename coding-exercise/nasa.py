7ed4089786e1c92409e46e8662c2dcd80496e8fb
atrox3d <atrox3d@gmail.com>
2022-12-20 17:58:37 +0100

    day 27: NASA exercise, teacher solution

--- coding-exercise/nasa.py
+++ coding-exercise/nasa.py
@@ -1,9 +1,10 @@
 import requests
 import streamlit as st
+from pathlib import Path
 
 API_KEY = 'DEMO_KEY'
 
-url = f'https://api.nasa.gov/planetary/apod?api_key={API_KEY}Y'
+url = f'https://api.nasa.gov/planetary/apod?api_key={API_KEY}'
 
 response = requests.get(url)
 data = response.json()
@@ -12,7 +13,15 @@ title = data['title']
 image_url = data['url']
 explanation = data['explanation']
 
+download_path = Path('downloads/')
+image_filename = Path(image_url).name
+image_path = download_path.joinpath(image_filename)
+response = requests.get(image_url)
+with open(image_path, 'wb') as file:
+    file.write(response.content)
+
 st.title(title)
-st.image(image=image_url, caption=explanation)
+st.image(image=str(image_path))
+st.write(explanation)
 
 data
